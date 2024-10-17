93e81dbd67f888a9dd3757254dfb04588be2d807
menvo <volemenn@gmail.com>
2024-10-17 23:56:00 +0700

    fix method get-data

--- src/db/config_db.rs
+++ src/db/config_db.rs
@@ -3,7 +3,7 @@ use tokio_postgres::NoTls;
 
 pub async fn configure_db_pool() -> Pool {
     let mut cfg = Config::new();
-    cfg.dbname = Some("rust-db".to_string());
+    cfg.dbname = Some("rust-ecommerce".to_string());
     cfg.user = Some("postgres".to_string());
     cfg.password = Some("1".to_string());
     cfg.host = Some("localhost".to_string());
--- src/main.rs
+++ src/main.rs
@@ -1,193 +1,89 @@
-use actix_web::{
-    get,
-    post,
-    put,
-    delete,
-    web,
-    App,
-    HttpRequest,
-    HttpResponse,
-    HttpServer,
-    Responder,
-    ResponseError,
-};
-use actix_web::http::header::ContentType;
-use actix_web::http::StatusCode;
-use actix_web::body::BoxBody;
+use actix_web::{web, App, HttpResponse, HttpServer, Responder, get};
+use deadpool_postgres::{Pool, Config, PoolError};
+use serde::{Serialize};
+use tokio_postgres::NoTls;
 
-use serde::{ Serialize, Deserialize };
-
-use std::fmt::Display;
-// use std::sync::Mutex;
-use deadpool_postgres::{Pool, Client};
-mod db;
-use db::config_db::configure_db_pool;
-mod model;
-use model::user::User;
-
-
-
-// Implement Responder Trait for Ticket
-impl Responder for User {
-    type Body = BoxBody;
-
-    fn respond_to(self, _req: &HttpRequest) -> HttpResponse<Self::Body> {
-        let res_body = serde_json::to_string(&self).unwrap();
-
-        // Create HttpResponse and set Content Type
-        HttpResponse::Ok().content_type(ContentType::json()).body(res_body)
-    }
+#[derive(Serialize)]
+struct User {
+    id: String,
+    username: String,
+    fullname: String,
+    password: String,
+    email: String,
+    phone_number: String,
+    role: Role,
+    is_active: Status,
 }
 
-#[derive(Debug, Serialize)]
-struct ErrNoId {
-    id: i32,
-    err: String,
+#[derive(Serialize)]
+enum Role {
+    Admin,
+    User
 }
 
-// Implement ResponseError for ErrNoId
-impl ResponseError for ErrNoId {
-    fn status_code(&self) -> StatusCode {
-        StatusCode::NOT_FOUND
-    }
-
-    fn error_response(&self) -> HttpResponse<BoxBody> {
-        let body = serde_json::to_string(&self).unwrap();
-        let res = HttpResponse::new(self.status_code());
-        res.set_body(BoxBody::new(body))
-    }
+#[derive(Serialize)]
+enum Status {
+    Active, 
+    InActive
 }
 
-// Implement Display for ErrNoId
-impl Display for ErrNoId {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:?}", self)
+async fn fetch_users(pool: &Pool<NoTls>) -> Result<Vec<User>, PoolError> {
+    let client = pool.get().await?;
+    let rows = client.query("SELECT id, username, fullname, password, email, phoneNumber, role::text, isActive::text FROM users", &[]).await?;
+
+    let mut results = Vec::new();
+    for row in rows {
+        let user = User {
+            id: row.get(0),
+            username: row.get(1),
+            fullname: row.get(2),
+            password: row.get(3),
+            email: row.get(4),
+            phone_number: row.get(5),
+            role: match row.get::<_, String>(6).as_str() {
+                "admin" => Role::Admin,
+                "user" => Role::User,
+                _ => panic!("Unknown role: {}", row.get::<_, String>(6)),
+            },
+            is_active: match row.get::<_, String>(7).as_str() {
+                "active" => Status::Active,
+                "inActive" => Status::InActive,
+                _ => panic!("Unknown role: {}", row.get::<_, String>(7)),
+            },
+        };
+        results.push(user);
     }
-}
 
-struct AppState {
-    db_pool: Pool
-}
-
-
-#[post("/add-user")]
-async fn post_ticket(data: web::Data<AppState>, user: web::Json<User>) -> impl Responder {
-    let client: Client = data.db_pool.get().await.unwrap();
-
-    let stmt = client.prepare("INSERT INTO users (id, author) VALUES ($1, $2)").await.unwrap();
-
-    match client.execute(&stmt, &[&user.id, &user.author]).await {
-        Ok(_) => HttpResponse::Created().finish(),
-        Err(err) => {
-            eprintln!("Error inserting ticket: {:?}", err);
-            HttpResponse::InternalServerError().finish()
-        }
-    }
+    Ok(results)
 }
 
 #[get("/users")]
-async fn get_tickets(data: web::Data<AppState>) -> impl Responder {
-    let client: Client = data.db_pool.get().await.unwrap();
-    let stmt = client.prepare("SELECT id, author FROM users").await.unwrap();
-
-    let rows = client.query(&stmt, &[]).await.unwrap();
-
-    let tickets: Vec<_> = rows.into_iter().map(|row| {
-        serde_json::json!({
-            "id": row.get::<_, i32>(0),
-            "author": row.get::<_, String>(1),
-        })
-    }).collect();
-
-    let response = serde_json::to_string(&tickets).unwrap();
-    HttpResponse::Ok().content_type(ContentType::json()).body(response)
-}
-
-// Get a ticket with the corresponding id
-#[get("/user/{id}")]
-async fn get_ticket(id: web::Path<i32>, data: web::Data<AppState>) -> Result<HttpResponse, ErrNoId> {
-    let client: Client = data.db_pool.get().await.unwrap();
-    let stmt = client.prepare("SELECT id, author FROM users WHERE id = $1").await.unwrap();
-
-    let rows = client.query(&stmt, &[&*id]).await.unwrap();
-
-    if let Some(row) = rows.get(0) {
-        let ticket = serde_json::json!({
-            "id": row.get::<_, i32>(0),
-            "author": row.get::<_, String>(1),
-        });
-        let response = serde_json::to_string(&ticket).unwrap();
-        Ok(HttpResponse::Ok().content_type(ContentType::json()).body(response))
-    } else {
-        Err(ErrNoId {
-            id: *id,
-            err: String::from("ticket not found"),
-        })
+async fn get_users(pool: web::Data<Pool<NoTls>>) -> impl Responder {
+    match fetch_users(&pool).await {
+        Ok(users) => HttpResponse::Ok().json(users),
+        Err(e) => HttpResponse::InternalServerError().body(format!("Error fetching users: {}", e)),
     }
 }
 
-// Update the ticket with the corresponding id
-#[put("/user/{id}")]
-async fn update_ticket(id: web::Path<i32>, data: web::Data<AppState>) -> Result<HttpResponse, ErrNoId> {
-    let client: Client = data.db_pool.get().await.unwrap();
+async fn configure_db_pool() -> Pool<NoTls> {
+    let mut cfg = Config::new();
+    cfg.dbname = Some("rust-ecommerce".to_string());
+    cfg.user = Some("postgres".to_string());
+    cfg.password = Some("1".to_string());
+    cfg.host = Some("localhost".to_string());
 
-    let stmt = client.prepare("UPDATE users SET author = $1 WHERE id = $2").await.unwrap();
-    let result = client.execute(&stmt, &[&"Updated Author", &*id]).await.unwrap();
-
-    if result > 0 {
-        let updated_ticket = serde_json::json!({
-            "id": *id,
-            "author": "Updated Author",
-        });
-        let response = serde_json::to_string(&updated_ticket).unwrap();
-        Ok(HttpResponse::Ok().content_type(ContentType::json()).body(response))
-    } else {
-        Err(ErrNoId {
-            id: *id,
-            err: String::from("ticket not found"),
-        })
-    }
-}
-
-// Delete the ticket with the corresponding id
-#[delete("/user/{id}")]
-async fn delete_ticket(id: web::Path<i32>, data: web::Data<AppState>) -> Result<HttpResponse, ErrNoId> {
-    let client: Client = data.db_pool.get().await.unwrap();
-
-    let stmt = client.prepare("DELETE FROM users WHERE id = $1 RETURNING id, author").await.unwrap();
-    let rows = client.query(&stmt, &[&*id]).await.unwrap();
-
-    if let Some(row) = rows.get(0) {
-        let deleted_ticket = serde_json::json!({
-            "id": row.get::<_, i32>(0),
-            "author": row.get::<_, String>(1),
-        });
-        let response = serde_json::to_string(&deleted_ticket).unwrap();
-        Ok(HttpResponse::Ok().content_type(ContentType::json()).body(response))
-    } else {
-        Err(ErrNoId {
-            id: *id,
-            err: String::from("ticket not found"),
-        })
-    }
+    cfg.create_pool(NoTls).unwrap()
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let pool = configure_db_pool().await;
-    let app_state = web::Data::new(AppState {
-        db_pool: pool
-    });
-
     HttpServer::new(move || {
         App::new()
-            .app_data(app_state.clone())
-            .service(post_ticket)
-            .service(get_ticket)
-            .service(get_tickets)
-            .service(update_ticket)
-            .service(delete_ticket)
+            .app_data(web::Data::new(pool.clone()))
+            .service(get_users)
     })
-        .bind(("127.0.0.1", 8000))?
-        .run().await
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
